7f62c0948a333d229d5bd3e43321e2b5c8afb7bc
MauroBorzi <mauroborzi91@gmail.com>
2025-07-18 15:34:57 +0200

    [upd]button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,28 @@
+// dichiaro la funzione per ciclare gli elementi generati dall'api
+const refreshEmail = () => {
+  for (i = 0; i < 10; i++) {
+    axios.get(direct).then((mail) => {
+      list.innerHTML += `<li class="list-group-item list-group-item-action">${mail.data.response}</li>`
+    })
+  }
+}
+
+
 // richiamo gli elementi dal DOM
 const list = document.getElementById(`list`)
+const button = document.getElementById(`refresh`)
 
 
 // dichiaro le variabili
 const direct = `https://flynn.boolean.careers/exercises/api/random/mail`
 
 
-// creo un ciclo for con al suo interno l'api
-for (i = 0; i < 10; i++) {
-  axios.get(direct).then((mail) => {
-    list.innerHTML += `<li class="list-group-item list-group-item-action">${mail.data.response}</li>`
-  })
-}
+// aggiorno le mail alla pressione del bottone
+button.addEventListener(`click`, () => {
+  list.innerHTML = ''
+  refreshEmail()
+})
+
+
+refreshEmail()
+
